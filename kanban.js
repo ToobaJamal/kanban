aec758d619c9d75b55295d49586156aaba7c1e73
Tooba <jtooba70@gmail.com>
2023-07-22 13:25:37 +0500

    Create kanban.js

---
+++ kanban.js
@@ -0,0 +1,25 @@
+export default class Kanban{
+    static getTasks(columnId) {
+
+    }
+    static insertTask(columnId, content) {
+
+    }
+    static deleteTask(taskId) {
+
+    }
+    static updateTask(taskId, updatedContent) {
+
+    }
+    static getAllTasks() {
+
+    }
+}
+
+function read() {
+
+}
+
+function save() {
+    
+}
