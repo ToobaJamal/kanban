623dcf1b9092e22e61a13a10975a5d9b6bee7268
Tooba <jtooba70@gmail.com>
2023-07-23 13:42:06 +0500

    Refactor kanban.js

--- kanban.js
+++ kanban.js
@@ -5,21 +5,29 @@ export default class Kanban{
     }
 
     static insertTask(columnId, content) {
-        const data = read()[columnId]
+        const data = read()
+        const column = data.find(column => {
+            return column.columnId == columnId
+        })
+        console.log(column)
         const task = {
             taskId: Math.floor(Math.random() * 100000),
             content: content 
         }
-        data.tasks.push(task)
+        console.log(task)
+        column.tasks.push(task)
         save(data)
+ 
         return task
     }
 
     static deleteTask(taskId) {
         const data = read()
         for(const column of data) {
-            const task = column.tasks.find(task => task.takId == taskId)
-            column.tasks.splice(column.tasks.indexOf(task), 1)
+            const task = column.tasks.find(task => task.taskId == taskId)
+            if(task) {
+                column.tasks.splice(column.tasks.indexOf(task), 1)
+            }        
         }
         save(data)
     }
@@ -47,26 +55,55 @@ export default class Kanban{
         targetColumn.tasks.push(task)
 
         save(data)
+
     }
 
     static getAllTasks() {
         const data = read()
+        columnCount()
         return [data[0].tasks, data[1].tasks, data[2].tasks]
+        
     }
 }
 
 function read() {
     const data = localStorage.getItem("data")
-    if(!data) {
+    try {
+        if (!data) {
+            return [
+                { columnId: 0, tasks: [] },
+                { columnId: 1, tasks: [] },
+                { columnId: 2, tasks: [] }
+            ]
+        }
+        return JSON.parse(data)
+    } catch (error) {
+        console.error("Error reading data from localStorage:", error)
         return [
-            {columnId: 0, tasks: []}, 
-            {columnId: 1, tasks: []}, 
-            {columnId: 2, tasks: []}
+            { columnId: 0, tasks: [] },
+            { columnId: 1, tasks: [] },
+            { columnId: 2, tasks: [] }
         ]
     }
-    return JSON.parse(data)
 }
+
 
 function save(data) {
     localStorage.setItem("data", JSON.stringify(data))
+    columnCount()
+}
+
+function columnCount() {
+    const data = read()
+    console.log(data[0].tasks)
+    const todo = document.querySelector("span.todo")
+    console.log(todo)
+    
+    todo.textContent = data[0].tasks.length
+
+    const pending = document.querySelector("span.pending")
+    pending.textContent = data[1].tasks.length
+
+    const completed = document.querySelector("span.completed")
+    completed.textContent = data[2].tasks.length
 }
