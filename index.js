3deed218e210ff1d48692864d7592c04de8c2de1
Tooba <jtooba70@gmail.com>
2023-07-25 13:22:11 +0500

    Format code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import Kanban from "./kanban.js";
 
-const todo = document.querySelector(".cards.todo")
-const pending = document.querySelector(".cards.pending")
-const completed= document.querySelector(".cards.completed")
-const cards = document.getElementsByClassName("card")
+const todo = document.querySelector(".cards.todo");
+const pending = document.querySelector(".cards.pending");
+const completed = document.querySelector(".cards.completed");
+const cards = document.getElementsByClassName("card");
 
-const taskbox = [todo, pending, completed]
+const taskbox = [todo, pending, completed];
 
 function addTaskCard(task, index) {
-    taskbox[index].innerHTML += `
+  taskbox[index].innerHTML += `
         <form class="card mb-4 bg-white rounded-md p-4 w-full flex flex-col" draggable="true" data-id="${task.taskId}">
         <input class="text-black font-normal" type="text" name="task" autocomplete="off" disabled="disabled" value="${task.content}"/>
         <span class="date self-center justify-self-center text-black">${task.date}</span>
@@ -30,113 +30,132 @@ function addTaskCard(task, index) {
             </span>
         </div>
     </form>
-        `
+        `;
 }
 
 Kanban.getAllTasks().forEach((tasks, index) => {
-    tasks.forEach(task => {
-        addTaskCard(task, index)
-    })
-})
+  tasks.forEach((task) => {
+    addTaskCard(task, index);
+  });
+});
 
-const addForm = document.querySelectorAll(".add") 
+const addForm = document.querySelectorAll(".add");
 
-addForm.forEach(form => {
-    form.addEventListener("submit", event => {
-        event.preventDefault()
-        if(form.task.value.trim() && form.date.value) {
-            const task = Kanban.insertTask(form.submit.dataset.id, form.task.value.trim(), form.date.value)
-            addTaskCard(task, form.submit.dataset.id)
-            form.reset()
-        }
-    })
-})
+addForm.forEach((form) => {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (form.task.value.trim() && form.date.value) {
+      const task = Kanban.insertTask(
+        form.submit.dataset.id,
+        form.task.value.trim(),
+        form.date.value,
+      );
+      addTaskCard(task, form.submit.dataset.id);
+      form.reset();
+    }
+  });
+});
 
-taskbox.forEach(column => {
-    column.addEventListener("click", event => {
-        event.preventDefault()
-        const card = event.target.closest(".card")
+taskbox.forEach((column) => {
+  column.addEventListener("click", (event) => {
+    event.preventDefault();
+    const card = event.target.closest(".card");
 
-        if (!card) return
+    if (!card) return;
 
-        const dateDisplay = card.querySelector('span');
-        const formInput = card.querySelector('input[name="task"]')
-        const dateInput = event.target.closest(".card").querySelector('input[name="date"]')
-        const editBtn = card.querySelector('.edit-btn')
-        const edit = card.querySelector('.edit')
-        const updateBtn = card.querySelector('.update')
-        const update = card.querySelector('.update-btn')
-        const deleteBtn = card.querySelector('.delete-btn')
+    const dateDisplay = card.querySelector("span");
+    const formInput = card.querySelector('input[name="task"]');
+    const dateInput = event.target
+      .closest(".card")
+      .querySelector('input[name="date"]');
+    const editBtn = card.querySelector(".edit-btn");
+    const edit = card.querySelector(".edit");
+    const updateBtn = card.querySelector(".update");
+    const update = card.querySelector(".update-btn");
+    const deleteBtn = card.querySelector(".delete-btn");
 
-        if(event.target.classList.contains("edit") || event.target.classList.contains("edit-btn")) {
-            formInput.removeAttribute("disabled")
-            dateInput.classList.remove("hide")
-            dateInput.value = dateDisplay.textContent.trim()
-            dateDisplay.classList.add("hide")
-            event.target.classList.add("hide")
-            editBtn.classList.add('hide')
-            updateBtn.classList.remove('hide')
-            update.classList.remove('hide')
+    if (
+      event.target.classList.contains("edit") ||
+      event.target.classList.contains("edit-btn")
+    ) {
+      formInput.removeAttribute("disabled");
+      dateInput.classList.remove("hide");
+      dateInput.value = dateDisplay.textContent.trim();
+      dateDisplay.classList.add("hide");
+      event.target.classList.add("hide");
+      editBtn.classList.add("hide");
+      updateBtn.classList.remove("hide");
+      update.classList.remove("hide");
 
-            dateInput.value = dateDisplay.textContent
-        }
+      dateInput.value = dateDisplay.textContent;
+    }
 
-        if(event.target.classList.contains("deadline-input")) {
-            dateInput.showPicker()
-        }
+    if (event.target.classList.contains("deadline-input")) {
+      dateInput.showPicker();
+    }
 
-        if(event.target.classList.contains("update") || event.target.classList.contains("update-btn")) {
-            formInput.setAttribute("disabled", "disabled")
-            updateBtn.classList.add("hide")
-            update.classList.add('hide')
-            editBtn.classList.remove("hide")
-            edit.classList.remove("hide")
-            dateInput.classList.add("hide")
-            dateDisplay.classList.remove("hide")
-            dateDisplay.textContent = dateInput.value
+    if (
+      event.target.classList.contains("update") ||
+      event.target.classList.contains("update-btn")
+    ) {
+      formInput.setAttribute("disabled", "disabled");
+      updateBtn.classList.add("hide");
+      update.classList.add("hide");
+      editBtn.classList.remove("hide");
+      edit.classList.remove("hide");
+      dateInput.classList.add("hide");
+      dateDisplay.classList.remove("hide");
+      dateDisplay.textContent = dateInput.value;
 
-            const taskId = update.dataset.id
-            const columnId = update.dataset.column
-            const content =formInput.value
-            const date = dateInput.value
-            Kanban.updateTask(taskId, {columnId, date, content})
-        }
+      const taskId = update.dataset.id;
+      const columnId = update.dataset.column;
+      const content = formInput.value;
+      const date = dateInput.value;
+      Kanban.updateTask(taskId, { columnId, date, content });
+    }
 
-        if(event.target.classList.contains("delete") || event.target.classList.contains("delete-btn")) {
-            formInput.parentElement.remove()
-            Kanban.deleteTask(deleteBtn.dataset.id)
-        }
-    })
+    if (
+      event.target.classList.contains("delete") ||
+      event.target.classList.contains("delete-btn")
+    ) {
+      formInput.parentElement.remove();
+      Kanban.deleteTask(deleteBtn.dataset.id);
+    }
+  });
 
-    column.addEventListener("dragstart", event => {
-        if(event.target.classList.contains("card")) {
-            event.target.classList.add("dragging")
-        }
-    })
+  column.addEventListener("dragstart", (event) => {
+    if (event.target.classList.contains("card")) {
+      event.target.classList.add("dragging");
+    }
+  });
 
-    column.addEventListener("dragover", event => {
-        const card = document.querySelector(".dragging")
-        column.appendChild(card)
-    })
+  column.addEventListener("dragover", (event) => {
+    const card = document.querySelector(".dragging");
+    column.appendChild(card);
+  });
 
-    column.addEventListener("dragend", event => {
-        if(event.target.classList.contains("card")) {
-            event.target.classList.remove("dragging")
-            Kanban.updateTask(event.target.dataset.id, {columnId: event.target.parentElement.dataset.id, date: event.target.querySelector('span').textContent, content: event.target.task.value})
-        }
-    })
-})
+  column.addEventListener("dragend", (event) => {
+    if (event.target.classList.contains("card")) {
+      event.target.classList.remove("dragging");
+      Kanban.updateTask(event.target.dataset.id, {
+        columnId: event.target.parentElement.dataset.id,
+        date: event.target.querySelector("span").textContent,
+        content: event.target.task.value,
+      });
+    }
+  });
+});
 
 function updateTaskCardStyle() {
-    const now = new Date();
-    for(const card of cards) {
-        const deadlineInput = card.querySelector('.date')
-        const deadline = new Date(deadlineInput.textContent)
-        const columnId = card.querySelector('.update-btn').dataset.column
-        if (columnId !== 2 && deadline < now) {
-            card.classList.add('deadline-passed')
-          }
+  const now = new Date();
+  for (const card of cards) {
+    const deadlineInput = card.querySelector(".date");
+    const deadline = new Date(deadlineInput.textContent);
+    const columnId = card.querySelector(".update-btn").dataset.column;
+    if (columnId !== 2 && deadline < now) {
+      card.classList.add("deadline-passed");
     }
   }
+}
 
-updateTaskCardStyle()
+updateTaskCardStyle();
--- kanban.js
+++ kanban.js
@@ -1,106 +1,105 @@
-export default class Kanban{
-    static getTasks(columnId) {
-        const data = read()
-        return data[columnId].tasks
-    }
+export default class Kanban {
+  static getTasks(columnId) {
+    const data = read();
+    return data[columnId].tasks;
+  }
 
-    static insertTask(columnId, content, date) {
-        const data = read()
-        const column = data.find(column => {
-            return column.columnId == columnId
-        })
-        const task = {
-            taskId: Math.floor(Math.random() * 100000),
-            date: date,
-            content: content 
+  static insertTask(columnId, content, date) {
+    const data = read();
+    const column = data.find((column) => {
+      return column.columnId == columnId;
+    });
+    const task = {
+      taskId: Math.floor(Math.random() * 100000),
+      date: date,
+      content: content,
+    };
+    column.tasks.push(task);
+    save(data);
+
+    return task;
+  }
+
+  static deleteTask(taskId) {
+    const data = read();
+    for (const column of data) {
+      const task = column.tasks.find((task) => task.taskId == taskId);
+      if (task) {
+        column.tasks.splice(column.tasks.indexOf(task), 1);
+      }
+    }
+    save(data);
+  }
+
+  static updateTask(taskId, updatedContent) {
+    const data = read();
+    function findColumnTask() {
+      for (const column of data) {
+        const task = column.tasks.find((task) => task.taskId == taskId);
+        if (task) {
+          return [task, column];
         }
-        column.tasks.push(task)
-        save(data)
- 
-        return task
+      }
     }
 
-    static deleteTask(taskId) {
-        const data = read()
-        for(const column of data) {
-            const task = column.tasks.find(task => task.taskId == taskId)
-            if(task) {
-                column.tasks.splice(column.tasks.indexOf(task), 1)
-            }        
-        }
-        save(data)
-    }
+    const [task, currentColumn] = findColumnTask();
 
-    static updateTask(taskId, updatedContent) {
-        const data = read()
-        function findColumnTask() {
-            for(const column of data) {
-                const task = column.tasks.find(task => task.taskId == taskId)
-                if(task) {
-                    return [task, column]
-                }
-            }
-        }
+    const targetColumn = data.find(
+      (column) => column.columnId == updatedContent.columnId,
+    );
 
-        const [task, currentColumn] = findColumnTask()
+    // update task
+    task.content = updatedContent.content;
+    task.date = updatedContent.date;
+    // update column
+    currentColumn.tasks.splice(currentColumn.tasks.indexOf(task), 1);
+    targetColumn.tasks.push(task);
 
-        const targetColumn = data.find(column => column.columnId == updatedContent.columnId)
+    save(data);
+  }
 
-        // update task
-        task.content = updatedContent.content
-        task.date = updatedContent.date
-        // update column
-        currentColumn.tasks.splice(currentColumn.tasks.indexOf(task), 1)
-        targetColumn.tasks.push(task)
-
-        save(data)
-
-    }
-
-    static getAllTasks() {
-        const data = read()
-        columnCount()
-        return [data[0].tasks, data[1].tasks, data[2].tasks]
-        
-    }
+  static getAllTasks() {
+    const data = read();
+    columnCount();
+    return [data[0].tasks, data[1].tasks, data[2].tasks];
+  }
 }
 
 function read() {
-    const data = localStorage.getItem("data")
-    try {
-        if (!data) {
-            return [
-                { columnId: 0, tasks: [] },
-                { columnId: 1, tasks: [] },
-                { columnId: 2, tasks: [] }
-            ]
-        }
-        return JSON.parse(data)
-    } catch (error) {
-        console.error("Error reading data from localStorage:", error)
-        return [
-            { columnId: 0, tasks: [] },
-            { columnId: 1, tasks: [] },
-            { columnId: 2, tasks: [] }
-        ]
+  const data = localStorage.getItem("data");
+  try {
+    if (!data) {
+      return [
+        { columnId: 0, tasks: [] },
+        { columnId: 1, tasks: [] },
+        { columnId: 2, tasks: [] },
+      ];
     }
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading data from localStorage:", error);
+    return [
+      { columnId: 0, tasks: [] },
+      { columnId: 1, tasks: [] },
+      { columnId: 2, tasks: [] },
+    ];
+  }
 }
 
-
 function save(data) {
-    localStorage.setItem("data", JSON.stringify(data))
-    columnCount()
+  localStorage.setItem("data", JSON.stringify(data));
+  columnCount();
 }
 
 function columnCount() {
-    const data = read()
-    const todo = document.querySelector("span.todo")
-    
-    todo.textContent = data[0].tasks.length
+  const data = read();
+  const todo = document.querySelector("span.todo");
 
-    const pending = document.querySelector("span.pending")
-    pending.textContent = data[1].tasks.length
+  todo.textContent = data[0].tasks.length;
 
-    const completed = document.querySelector("span.completed")
-    completed.textContent = data[2].tasks.length
+  const pending = document.querySelector("span.pending");
+  pending.textContent = data[1].tasks.length;
+
+  const completed = document.querySelector("span.completed");
+  completed.textContent = data[2].tasks.length;
 }
